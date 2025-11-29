fd4d27cf909071be830c888596688b8bad71fc39
Mahmadh Nooriddhin <nooruddinmd13@gmail.com>
2025-11-29 13:12:38 +0530

    Delete Patient.js

--- Patient.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const patientSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    disease: String
-});
-
-export default mongoose.model("Patient", patientSchema);
